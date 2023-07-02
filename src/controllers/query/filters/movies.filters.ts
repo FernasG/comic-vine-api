decaf6bbe938a7d870b46f8f2db9f5aaf8c71908
FernasG <fernandogoul7@gmail.com>
2023-07-02 00:09:42 -0300

    feat: add parsers

--- src/controllers/query/filters/movies.filters.ts
+++ src/controllers/query/filters/movies.filters.ts
@@ -1,7 +1,86 @@
+import { DataSource } from "typeorm";
+
 // Movies-----------------------------------------------------
 // 3 → Filmes com os maiores custos de produção
 // 4 → Filmes com os menores custos de produção
-// 5 → filmes com as melhores avaliações
-// 6 → filmes com as piores avaliações
-// 7 → filmes com a maior duração
-// 8 → filmes com a menor duração
+
+export const MoviesHigherDuration = (async (connection: DataSource) => {
+    try {
+        const movies = await connection.createQueryRunner().query(`
+            SELECT id, name, runtime FROM movies WHERE runtime IS NOT NULL
+            ORDER BY CAST(runtime AS INTEGER) DESC LIMIT 10;
+        `);
+
+        return { statusCode: 200, data: { movies } };
+    } catch (err: any) {
+        return { statusCode: 500, data: { message: err.message } };
+    }
+});
+
+
+export const MoviesLowerDuration = (async (connection: DataSource) => {
+    try {
+        const movies = await connection.createQueryRunner().query(`
+            SELECT id, name, runtime FROM movies WHERE runtime IS NOT NULL
+            ORDER BY CAST(runtime AS INTEGER) ASC LIMIT 10;
+        `);
+
+        return { statusCode: 200, data: { movies } };
+    } catch (err: any) {
+        return { statusCode: 500, data: { message: err.message } };
+    }
+});
+
+export const MoviesHigherRevenue = (async (connection: DataSource) => {
+    try {
+        const movies = await connection.createQueryRunner().query(`
+            SELECT id, name, total_revenue FROM movies WHERE total_revenue IS NOT NULL
+            ORDER BY CAST(total_revenue AS BIGINT) DESC LIMIT 10;
+        `);
+
+        return { statusCode: 200, data: { movies } };
+    } catch (err: any) {
+        return { statusCode: 500, data: { message: err.message } };
+    }
+});
+
+
+export const MoviesLowerRevenue = (async (connection: DataSource) => {
+    try {
+        const movies = await connection.createQueryRunner().query(`
+            SELECT id, name, total_revenue FROM movies WHERE total_revenue IS NOT NULL
+            ORDER BY CAST(total_revenue AS BIGINT) ASC LIMIT 10;
+        `);
+
+        return { statusCode: 200, data: { movies } };
+    } catch (err: any) {
+        return { statusCode: 500, data: { message: err.message } };
+    }
+});
+
+export const MoviesHigherBudget = (async (connection: DataSource) => {
+    try {
+        const movies = await connection.createQueryRunner().query(`
+            SELECT id, name, budget FROM movies WHERE budget IS NOT NULL
+            ORDER BY CAST(budget AS BIGINT) DESC LIMIT 10;
+        `);
+
+        return { statusCode: 200, data: { movies } };
+    } catch (err: any) {
+        return { statusCode: 500, data: { message: err.message } };
+    }
+});
+
+
+export const MoviesLowerBudget = (async (connection: DataSource) => {
+    try {
+        const movies = await connection.createQueryRunner().query(`
+            SELECT id, name, budget FROM movies WHERE budget IS NOT NULL
+            ORDER BY CAST(budget AS BIGINT) ASC LIMIT 10;
+        `);
+
+        return { statusCode: 200, data: { movies } };
+    } catch (err: any) {
+        return { statusCode: 500, data: { message: err.message } };
+    }
+});
