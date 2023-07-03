0c17acd8a3625255c45f7616a1874f3f6ec446c6
andre <andreluccag@gmail.com>
2023-07-03 00:09:21 -0300

    add cors

--- App.ts
+++ App.ts
@@ -5,9 +5,11 @@ import router from "./src/routes";
 
 (async () => {
     const app = express();
+    var cors = require('cors')
     const PORT = process.env.PORT || 3000;
 
     app.use(express.json());
+    app.use(cors())
     app.use(router);
 
     app.listen(PORT, () => { console.log(`Server running - http://localhost:${PORT}`) });
